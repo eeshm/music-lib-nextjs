3634ee811768226d810ca8267f4f0aca74ba3880
eeshmidha <eeshmidha80@gmail.com>
2025-02-02 00:05:09 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+ 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -18,15 +19,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children,}: Readonly<{children: React.ReactNode;}>)
+  {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="dark">
+      <body 
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+         <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+         </div>
         {children}
       </body>
     </html>
---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,56 @@
+'use client'
+import Link from "next/link"
+import courseData from "@/data/music_courses.json"
+import { BackgroundGradient } from "./ui/background-gradient"
+
+interface Course{
+        id: number,
+        title  : string,
+        slug: string, 
+        description:string,
+        price: number,
+        instructor: string,
+        isFeatured: boolean,
+        image: string 
+}
+export default function FeaturedCourses(){
+    const featuredCourses= courseData.courses
+     .filter((course:Course)=> course.isFeatured)
+    return(
+        <div className="py-12 bg-gray-900">
+            <div>
+                <div className="text-center">
+                    <h2 className="text-base text-teal-800 font-semibold
+                    tracking-wide uppercase">FEATURED COURSES</h2>
+                    <p className="mt-2 text-3xl leading-8
+                    font-extrabold tracking-tight bg-clip-text text-transparent bg-gradient-to-b
+                    from-neutral-50 to-neutral-400 sm:text-4xl">Learn With the Best</p></div>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 
+            lg:grid-cols-3 gap-8 justify-center">
+                {featuredCourses.map((course:Course)=>(
+                    <div key={course.id} className="flex justify-center mt-6" >
+                        <BackgroundGradient className="flex flex-col rounded-[22px] bg-white
+                         dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                            <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                            <p className="text-lg sm:text-xl
+                            text-black mt-4 mb-2 dark:text-neutral-200 text-center text-">{course.title}</p>
+                            <p className="text-sm text-neutral-600
+                             dark:text-neutral-400 flex-grow text-center">{course.description}</p>
+                            <Link href={`/courses/${course.slug}`} className="text-white mt-6">Learn More</Link>
+                            </div>
+                        </BackgroundGradient>
+                    </div>
+                ))}
+            </div>
+            <div className="mt-20 text-center">
+                <Link href={"/courses"}
+                className="px-4 py-2 rounded border border-neutral-500
+                 text-gray-400 bg-gray-900 hover:bg-gray-100 
+                 transition duration-200">
+                 View All Courses
+                </Link>
+            </div>
+        </div> 
+    )
+}
